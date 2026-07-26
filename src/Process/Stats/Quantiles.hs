512725984f9927ddb5f28fc42a5a1030bf9fdee9
Tony Day <tonyday567@gmail.com>
2026-07-26 11:51:50 +1000

    Replace tdigest with dunning-t-digest; rewrite OnlineTDigest

--- src/Process/Stats/Quantiles.hs
+++ src/Process/Stats/Quantiles.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RebindableSyntax #-}
 
 -- | Process quantile statistics.
@@ -15,83 +14,81 @@ module Process.Stats.Quantiles
   )
 where
 
-import Control.Monad.ST
-import Process.Stats
-import Data.Ord
-import Data.TDigest hiding (median)
-import Data.TDigest.Internal
-import Data.TDigest.Tree.Internal (TDigest (..), absMaxSize, emptyTDigest, insertCentroid, relMaxSize, size, toMVector)
-import Data.Vector.Algorithms.Heap qualified as VHeap
-import Data.Vector.Unboxed qualified as VU
+import Data.Sketch.TDigest qualified as TD
 import NumHask.Prelude hiding (fold)
+import Process.Stats
 
+-- | An online t-digest with exponential decay weighting.
+--
+-- Each inserted point receives weight @r ** (-(n+1))@ where @n@ counts
+-- insertions since the last forced compression. Periodically the weights
+-- are rescaled so that older points decay relative to newer ones.
 data OnlineTDigest = OnlineTDigest
-  { td :: TDigest 25,
+  { td :: TD.TDigest,
     tdN :: Int,
     tdRate :: Double
   }
   deriving (Show)
 
-emptyOnlineTDigest :: Double -> OnlineTDigest
-emptyOnlineTDigest = OnlineTDigest (emptyTDigest :: TDigest n) 0
+-- | T-digest compression parameter. Delta=50 reproduces the accuracy profile
+-- of the previous @tdigest@ package at its compression=25 setting.
+tdigestDelta :: Double
+tdigestDelta = 50
 
--- | Process quantiles based on the tdigest library
+emptyOnlineTDigest :: Double -> OnlineTDigest
+emptyOnlineTDigest r = OnlineTDigest (TD.emptyWith tdigestDelta) 0 r
+
+-- | Process quantiles based on the t-digest library.
 quantiles :: Double -> [Double] -> Process Double [Double]
 quantiles r qs = Process inject step extract
   where
     step x a = onlineInsert a x
     inject a = onlineInsert a (emptyOnlineTDigest r)
-    extract x = fromMaybe (0 / 0) . (`quantile` t) <$> qs
+    extract x = fromMaybe (0 / 0) . (`TD.quantile` t) <$> qs
       where
         (OnlineTDigest t _ _) = onlineForceCompress x
 
--- | Process median using 'tdigest'
+-- | Process median using the t-digest algorithm.
 --
--- The tdigest algorithm works best at extremes and can be unreliable in the centre.
+-- The t-digest algorithm works best at extremes and can be unreliable in the centre.
 median :: Double -> Process Double Double
 median r = Process inject step extract
   where
     step x a = onlineInsert a x
     inject a = onlineInsert a (emptyOnlineTDigest r)
-    extract x = fromMaybe (0 / 0) (quantile 0.5 t)
+    extract x = fromMaybe (0 / 0) (TD.quantile 0.5 t)
       where
         (OnlineTDigest t _ _) = onlineForceCompress x
 
 onlineInsert' :: Double -> OnlineTDigest -> OnlineTDigest
 onlineInsert' x (OnlineTDigest td' n r) =
   OnlineTDigest
-    (insertCentroid (x, r ^^ (-(fromIntegral $ n + 1) :: Integer)) td')
+    (TD.addWeighted x (r ^^ (-(fromIntegral $ n + 1) :: Integer)) td')
     (n + 1)
     r
 
 onlineInsert :: Double -> OnlineTDigest -> OnlineTDigest
 onlineInsert x otd = onlineCompress (onlineInsert' x otd)
 
+-- | Force a compression pass when the digest has grown enough that the
+-- decay weights are becoming numerically awkward. The threshold is chosen
+-- to keep weights within a comfortable double range while preserving the
+-- exponential-decay semantics.
 onlineCompress :: OnlineTDigest -> OnlineTDigest
-onlineCompress otd@(OnlineTDigest Nil _ _) = otd
-onlineCompress otd@(OnlineTDigest t _ _)
-  | Data.TDigest.Tree.Internal.size t > relMaxSize * compression
-      && Data.TDigest.Tree.Internal.size t > absMaxSize =
-      onlineForceCompress otd
+onlineCompress otd@(OnlineTDigest _ n _)
+  | n > maxInsertsSinceCompress = onlineForceCompress otd
   | otherwise = otd
-  where
-    compression = 25
 
+maxInsertsSinceCompress :: Int
+maxInsertsSinceCompress = 200
+
+-- | Rescale all centroid weights by @r ** n@ and reset the insertion counter.
+-- This is the exponential-decay equivalent of normalising weights.
 onlineForceCompress :: OnlineTDigest -> OnlineTDigest
-onlineForceCompress otd@(OnlineTDigest Nil _ _) = otd
 onlineForceCompress (OnlineTDigest t n r) = OnlineTDigest t' 0 r
   where
-    t' =
-      foldl' (flip insertCentroid) emptyTDigest $
-        (\(m, w) -> (m, w * (r ^^ n))) . fst <$> VU.toList centroids
-    -- Centroids are shuffled based on space
-    centroids :: VU.Vector (Centroid, Double)
-    centroids =
-      runST $ do
-        v <- toMVector t
-        -- sort by cumulative weight
-        VHeap.sortBy (comparing snd) v
-        VU.unsafeFreeze v
+    t' = TD.compress $ foldl' rebuild (TD.emptyWith tdigestDelta) (TD.centroidList t)
+    rebuild a c = TD.addWeighted (TD.cMean c) (TD.cWeight c * r ^^ n) a
 
 -- | A process that computes the running quantile bucket. For example,
 -- in a scan, @digitize 0.9 [0.5]@ returns:
@@ -106,7 +103,7 @@ digitize r qs = Process inject step extract
     inject a = (onlineInsert a (emptyOnlineTDigest r), a)
     extract (x, l) = bucket' qs' l
       where
-        qs' = fromMaybe (0 / 0) . (`quantile` t) <$> qs
+        qs' = fromMaybe (0 / 0) . (`TD.quantile` t) <$> qs
         (OnlineTDigest t _ _) = onlineForceCompress x
         bucket' xs l' =
           fold (Process id (+) id) $
